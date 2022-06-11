2b208ab5d1ce1bb4151554a86d1be3a814802aff
YaSh_ <yashverma30115@gmail.com>
2022-06-11 19:15:20 +0530

    please

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -54,7 +54,9 @@ function HeroHome() {
           <div>
             <div className="relative flex justify-center mb-8" data-aos="zoom-y-out" data-aos-delay="450">
               <div className="flex flex-col justify-center">
-                <img className="mx-auto" src="https://pbs.twimg.com/media/FSnrB6rWQAAPPA4.jpg" height="432" alt="Hero" />
+                <video className="mx-auto" autoPlay={true} alt="Hero" >
+                  <source src="https://drive.google.com/uc?export=view&id=1gtg5aa8GyqfNet6jy4RyLpPIoUNd62xr" type='video/mp4' />
+                </video>
                 {/* <svg className="absolute inset-0 max-w-full mx-auto md:max-w-none h-auto" width="768" height="432" viewBox="0 0 768 432" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
                   <defs>
                     <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="hero-ill-a">
@@ -89,7 +91,7 @@ function HeroHome() {
                   <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
                   <path d="M10 17l6-5-6-5z" />
                 </svg>
-                <span className="ml-3">Watch the full video (2 min)</span>
+                <span className="ml-3">Watch the full video</span>
               </button> */}
             </div>
 
